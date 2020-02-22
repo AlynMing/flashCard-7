5dc1779dff23321956e973b51d83bab58ef20496
Roseline Shapi <roseshapi81@gmail.com>
2020-02-21 21:52:17 -0600

    changed color of labels

--- flashCard/ViewController.swift
+++ flashCard/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backLabel: UILabel!
+    
+    
+    @IBOutlet weak var frontLabel: UILabel!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func didTapOnCard(_ sender: Any) {
+        if(self.frontLabel.isHidden == false){
+            self.frontLabel.isHidden = true
+        }
+        else{
+            self.frontLabel.isHidden = false
+        }
+    }
 }
 
